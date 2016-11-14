52bd4758218a320556b22b6162c457bf0275973b
zhaoxm <zhao_xin_ming@outlook.com>
2016-11-14 21:08:07 +0800

    从1940年开始

--- spider.js
+++ spider.js
@@ -5,14 +5,16 @@ const url = require('url');
 function getMovie(urlLink, save) {
     superagent.get(urlLink)
     .set({
-        'Referer': 'https://movie.douban.com/subject_search?search_text=%E5%86%B0%E8%A1%80%E6%9A%B4&cat=1002',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36',
-        'Host': 'movie.douban.com',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate, sdch, br',
         'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
+        'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
-        'Cookie': 'll="118124"; bid=fn-RmlYyWng; _ga=GA1.2.1994921951.1470050267; ap=1; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1478574935%2C%22https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3D28Uk4fXLf-6NI3cK1A3Kr66jWlNm1Dwh_az-ho_I7pelabNnVLmEztuHx_EI25Rv%26wd%3D%26eqid%3Da1835db300085b5600000003581f1a02%22%5D; _vwo_uuid_v2=B7BF2419FAC162BC07624ED70DA4E016|ca9450b179ebb65ef21ff14e960a416e; _pk_id.100001.4cf6=3a84fe7fdecfb7ec.1478178997.9.1478575923.1478572897.; _pk_ses.100001.4cf6=*; __utma=30149280.1994921951.1470050267.1478570364.1478574935.10; __utmb=30149280.0.10.1478574935; __utmc=30149280; __utmz=30149280.1478433289.3.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utma=223695111.1992274825.1478178997.1478570364.1478574935.9; __utmb=223695111.0.10.1478574935; __utmc=223695111; __utmz=223695111.1478433289.2.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic'
+        'Cookie': 'll="118124"; bid=fn-RmlYyWng; _ga=GA1.2.1994921951.1470050267; ct=y; __ads_session=zF3nPylR0Aha7pkFiwA=; ap=1; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1479051883%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _vwo_uuid_v2=B7BF2419FAC162BC07624ED70DA4E016|ca9450b179ebb65ef21ff14e960a416e; _pk_id.100001.4cf6=3a84fe7fdecfb7ec.1478178997.20.1479052900.1479048923.; _pk_ses.100001.4cf6=*; __utma=30149280.1994921951.1470050267.1479043908.1479051883.21; __utmb=30149280.0.10.1479051883; __utmc=30149280; __utmz=30149280.1478594157.12.3.utmcsr=movie.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utma=223695111.1992274825.1478178997.1479043908.1479051883.19; __utmb=223695111.0.10.1479051883; __utmc=223695111; __utmz=223695111.1478917251.16.4.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/',
+        'Host': 'movie.douban.com',
+        'Referer': 'https://movie.douban.com/tag/',
+        'Upgrade-Insecure-Requests': 1,
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36',
     })
     .end(function(err, res) {
         if (err) {
